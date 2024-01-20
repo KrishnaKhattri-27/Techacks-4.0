81aa69ab8c79552a3ce865fcb347d20bf1b09d05
Krishna <krishnakhattri24@gmail.com>
2024-01-20 16:17:10 +0530

    Navbar

--- client/src/page/Dashboard.jsx
+++ client/src/page/Dashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
 
 const Dashboard = () => {
   return (
-    <div>
-      
+    <div className='h-screen bg-black'>
+      <Navbar/>
     </div>
   )
 }
