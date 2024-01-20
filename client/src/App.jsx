3970f204f3acba268f8171b469877f071320e197
Krishna <krishnakhattri24@gmail.com>
2024-01-21 02:43:29 +0530

    minor

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,11 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 
 function App() {
   const nav = useNavigate();
-
+  const [status, setStatus] = useState(false);
+  const [messages, setMessages] = useState({
+    img: "",
+    title: "",
+  });
   const [isloggedin, setLoggedin] = useState(false);
 
   useEffect(() => {
@@ -33,12 +37,13 @@ function App() {
       console.log(json.error);
     }
   };
-  const [status, setStatus] = useState(false);
+  const alert_message =
+  "WARNING..!! Suspicious Activity Detected. Confirm if the detected emergence is an actual emergency or not.";
   useEffect(() => {
     const socket = io("http://localhost:8000");
     socket.on("messageFromServer", (message, image_url) => {
       console.log("socket",message, image_url);
-      setStatus({
+      setMessages({
         img: image_url,
         title: message,
       });
@@ -65,6 +70,31 @@ function App() {
   //   }
   // }, []);
 
+  const yesHandler = async (e) => {
+    let sub, cont;
+    if (e === "YES") {
+      sub = "Immediate Alert - Confirmed Suspicious Activity";
+      cont = `Dear [Higher Authority's Name],\n\nI urgently report the detection of suspicious activity by our AI surveillance system. [Monitor's Name] has verified and marked the activity as confirmed, triggering an alarm in the designated.\nPlease find the attached suspicious image. area\n\nRegards\nTechnical Team\n\nLink:${messages.img}
+        `;
+    } else {
+      sub = "Clarification - Resolved Alert on Detected Suspicious Activity";
+      cont = `"Dear [Higher Authority's Name],\n\nI wish to update you that our AI surveillance system initially detected suspicious activity. However, upon review, [Monitor's Name] has marked the activity as a false positive. No further action is required at this time\nPlease find the attached suspicious image. area\n\nRegards\nTechnical Team\n\nLink:${messages.img}`;
+    }
+    const temp = { sub: sub, cont: cont };
+    const response = await fetch("http://localhost:8000/v1/sendMail", {
+      method: "POST",
+      body: JSON.stringify(temp),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      console.log(`HTTP error! Status: ${response.status}`);
+    }
+    setStatus(false)
+    return response.json();
+  };
+
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
@@ -72,7 +102,7 @@ function App() {
         path="/login"
         element={<Login isloggedin={isloggedin} passData={passData} />}
       />
-      <Route path="/police" element={<Police />} />
+      <Route path="/police" element={<Police messages={messages} status={status} yesHandler={yesHandler}  />} />
       <Route path="/traffic" element={<Traffic />} />
     </Routes>
   );
