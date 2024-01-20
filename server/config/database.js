6ca46d8c13baa8657183a2315237acb16d477321
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-01-21 00:05:37 +0530

    Added Mongo

--- server/config/database.js
+++ server/config/database.js
@@ -6,13 +6,7 @@ require("dotenv").config();
 const dbconnect = () => {
   mongoose
     .connect(
-      "mongodb+srv://dhruvchandak5:IUC3raqyzYuPkvkp@recognition.wyyroyh.mongodb.net/?retryWrites=true&w=majority",
-      {
-        // useNewUrlParser: true,
-        // useUnifiedTopology: true,
-        // serverSelectionTimeoutMS: 30000, // 30 seconds
-        // socketTimeoutMS: 45000, // 45 seconds
-      }
+      "mongodb+srv://dhruvchandak5:IUC3raqyzYuPkvkp@recognition.wyyroyh.mongodb.net/?retryWrites=true&w=majority"
     )
     .then(() => {
       console.log("database connected");
--- server/server.js
+++ server/server.js
@@ -2,20 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 // const routes = require("./routes/userRoutes");
 const http = require("http");
-const connectDB = require("./utils/database");
 const cors = require("cors");
-// const { Server } = require("socket.io"); // Import Server from socket.io
+const { Server } = require("socket.io"); // Import Server from socket.io
 const dbconnect = require("./config/database");
 require("./models/PrisonerData");
 const mongoose = require("mongoose");
 const app = express();
 const server = http.createServer(app);
-// const io = new Server(server, {
-//   cors: {
-//     origin: ["http://localhost:3000"],
-//     methods: ["GET", "POST"],
-//   },
-// });
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+  },
+});
 
 app.use(
   cors({
@@ -26,28 +25,21 @@ app.use(
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use("/v1", routes);
+app.use("/v1", routes);
 
 // Socket.IO connection event
-// io.on("connection", (socket) => {
-//   console.log("A user connected");
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
-//   socket.on("messageFromFrontend", (message) => {
-//     console.log("Message from frontend:", message);
-//   });
+  socket.on("messageFromFrontend", (message) => {
+    console.log("Message from frontend:", message);
+  });
 
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected");
-//   });
-// });
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
 
-// app.post("/api/receiveImageChunk", (req, res) => {
-//   console.log(req.body.image_url, req.body.result);
-//   io.emit("messageFromServer", req.body.result, req.body.image_url);
-//   res.json({
-//     message: "Image received on the server",
-//   });
-// });
 const PrisonerData = mongoose.model("Prisoner");
 
 app.post("/api/recievePrisoner", (req, res) => {
@@ -76,17 +68,17 @@ app.post("/api/recievePrisoner", (req, res) => {
   }
 });
 
-// app.post("/api/recieveTrafficNumber", (req, res) => {
-//   console.log(req.body.number);
-//   io.emit("messageFromTraffic", req.body.number);
-//   res.json({
-//     message: "Traffic Number received on the server",
-//   });
-// });
+app.post("/api/recieveTrafficNumber", (req, res) => {
+  console.log(req.body.number);
+  io.emit("messageFromTraffic", req.body.number);
+  res.json({
+    message: "Traffic Number received on the server",
+  });
+});
 
 app.post("/api/recieveActivityPrediction", (req, res) => {
   console.log(req.body.prediction);
-  // io.emit("messageFromActivity", req.body.prediction);
+  io.emit("messageFromActivity", req.body.prediction);
   res.json({
     message: "ACtivity predictions received on the server",
   });
