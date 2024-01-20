bc83c42b4486d55e681630767ae964edea8383fd
Krishna <krishnakhattri24@gmail.com>
2024-01-20 17:11:11 +0530

    bug

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Dashboard from "./page/Dashboard";
 import Login from "./page/Login"
@@ -5,14 +6,26 @@ import Police from "./page/Police"
 import Traffic from "./page/Traffic";
 import {Routes,Route} from "react-router-dom"
 
+
 function App() {
+
+  const [isloggedin, setLoggedin] = useState(false);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const name=e.target.name.value;
+    // const password=e.target.password.value;
+    // setUserData({name,password})
+    setLoggedin(true)
+  };
+
   return (
     <Routes>
     <Route path="/" element={<Dashboard />} />
     <Route
       path="/login"
       element={
-        <Login  />
+        <Login handleSubmit={handleSubmit} isloggedin={isloggedin} />
       }
     />
     <Route
